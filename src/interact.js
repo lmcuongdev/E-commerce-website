dba906fccabdc46f0241c836c8f65a9ef06cdb84
leduchuy225 <56461047+leduchuy225@users.noreply.github.com>
2020-04-13 23:16:45 +0700

    Add submit-form
    Khi ấn vào sign-up xuất hiện bảng để đăng nhập

--- src/interact.js
+++ src/interact.js
@@ -11,27 +11,35 @@
 // refactor to
 // Nav bar responsive interact
 $(document).ready(() => {
-  $("#menu-btn").on("click", () => {
-    $("#menu-btn").fadeOut(100);
-    setTimeout(() => $("#close-btn").fadeIn(100), 100);
-    $("#side-menu").slideDown("fast");
-  });
+    $("#menu-btn").on("click", () => {
+        $("#menu-btn").fadeOut(100);
+        setTimeout(() => $("#close-btn").fadeIn(100), 100);
+        $("#side-menu").slideDown("fast");
+    });
 
-  $("#close-btn").on("click", () => {
-    $("#close-btn").fadeOut(100);
-    setTimeout(() => $("#menu-btn").fadeIn(100), 100);
-    $("#side-menu").slideUp("fast");
-  });
-  // fix responsive nav bar
-  const media = window.matchMedia("(max-width: 980px)");
-  media.addEventListener("change", ({ matches }) => {
-    $("#close-btn").hide();
-    if (matches) {
-      $("#menu-btn").show();
-      $("#side-menu").hide();
-    } else {
-      $("#menu-btn").hide();
-      $("#side-menu").show();
-    }
-  });
+    $("#close-btn").on("click", () => {
+        $("#close-btn").fadeOut(100);
+        setTimeout(() => $("#menu-btn").fadeIn(100), 100);
+        $("#side-menu").slideUp("fast");
+    });
+    // fix responsive nav bar
+    const media = window.matchMedia("(max-width: 980px)");
+    media.addEventListener("change", ({ matches }) => {
+        $("#close-btn").hide();
+        if (matches) {
+            $("#menu-btn").show();
+            $("#side-menu").hide();
+        } else {
+            $("#menu-btn").hide();
+            $("#side-menu").show();
+        }
+    });
 });
+
+$("#sign-up").click(function() {
+    $("#form-group").css('display', 'block');
+});
+
+$("#form-group span").click(function() {
+    $("#form-group").css('display', 'none');
+})
