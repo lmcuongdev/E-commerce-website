b63d63052407dd7813442bef1feaf1d753b27fdb
erwgregreg <trongthuong.uet@gmail.com>
2020-04-14 08:51:27 +0700

    add filter and scroll to top page

--- src/interact.js
+++ src/interact.js
@@ -35,3 +35,45 @@ $(document).ready(() => {
     }
   });
 });
+
+/*------------------------Goto Top Button---------------*/
+$(document).ready(function(){
+  $(window).scroll(function () {
+         if ($(this).scrollTop() > 500) {
+             $('#back-to-top').fadeIn();
+         } else {
+             $('#back-to-top').fadeOut();
+         }
+     });
+     // scroll body to 0px on click
+     $('#back-to-top').click(function () {
+         $('#back-to-top').tooltip('hide');
+         $('body,html').animate({
+             scrollTop: 0
+         }, 800);
+         return false;
+     });
+     
+     $('#back-to-top').tooltip('show');
+
+});
+
+
+$(document).ready(function(){
+
+  $(".filter-button").click(function(){
+      var value = $(this).attr('data-filter');
+      
+      if(value == "all")
+      {
+          $('.filter').show('1000');
+      }
+      else
+      {
+          $(".filter").not('.'+value).hide('3000');
+          $('.filter').filter('.'+value).show('3000');
+          
+      }
+  });
+
+});
