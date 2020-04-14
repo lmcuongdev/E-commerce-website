d889bb34ee7366eaf1b191e94538c81f47e21012
leduchuy225 <56461047+leduchuy225@users.noreply.github.com>
2020-04-14 11:37:32 +0700

    Filter Products
    Tìm kiếm sản phẩm có trong danh mục

--- src/interact.js
+++ src/interact.js
@@ -42,4 +42,38 @@ $("#sign-up").click(function() {
 
 $("#form-group span").click(function() {
     $("#form-group").css('display', 'none');
+});
+
+$("document").ready(function() {
+    var name = [];
+    $(".side-menu ul li ul li").each(function() {
+        name.push($(this).text());
+    });
+    for (i = 0; i < name.length; i++) {
+        $("#sel").append('<option>' + name[i] + '</option>');
+    }
+});
+
+$("document").ready(function() {
+    $("#myInput").on("keyup", function() {
+        var seacrhName = $(this).val().toLowerCase();
+        var countName = 0;
+        $("#sel option").filter(function() {
+            var indexName = $(this).text().toLowerCase().indexOf(seacrhName);
+            if (indexName > -1) countName++;
+            if (countName == 0) {
+                $("#sel").css('display', 'none');
+            } else {
+                if ($("#myInput").val() != "") {
+                    $("#sel").css('display', 'block');
+                    if (countName <= 3) {
+                        $("#sel").attr('size', (countName + 1));
+                    } else {
+                        $("#sel").attr('size', '4');
+                    }
+                }
+            }
+            $(this).toggle(indexName > -1);
+        });
+    });
 })
