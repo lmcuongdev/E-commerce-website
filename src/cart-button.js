a8ebfb185b3c59f597e253a13b1ef4ed56cdc7f3
leduchuy225 <56461047+leduchuy225@users.noreply.github.com>
2020-04-15 18:34:45 +0700

    Fix Cart-box

--- src/cart-button.js
+++ src/cart-button.js
@@ -7,13 +7,25 @@ $("#open-side").on('click', function() {
 });
 
 $(document).ready(function() {
-    var addMoney, subMoney;
     var sum = 0;
 
+    var listOfProduct = [];
+
     $(".on-sale .fa-shopping-cart").on('click', function() {
-        var product = $(this).parent().parent().parent().siblings();
-        $(".cart-list").prepend('<li>' + product.html() + '</li>');
-        addMoney = $("h5").html();
+        var product = $(this).parent().parent().parent().siblings("div");
+        name = $.trim($(product).children("h3").text());
+        var index = listOfProduct.indexOf(name);
+        if (index === -1) {
+            $(".cart-list").prepend('<li>' + '<span id="quantityInBag" class="float-right" style="font-size: larger; color: red;">1</span>' + product.html() + '</li>');
+            listOfProduct.push(name);
+        } else {
+            $(".cart-list li h3").each(function() {
+                if ($(this).text() === name) {
+                    $(this).siblings("span").text(parseInt($(this).siblings("span").text()) + 1);
+                }
+            });
+        }
+        var addMoney = $(product).children("h5").html();
         sum += parseFloat(addMoney.substr(1));
         $('.total .float-right').text(sum);
 
@@ -21,10 +33,18 @@ $(document).ready(function() {
     });
 
     $(document).on("click", ".cart-list li[class!='total ']", function() {
-        subMoney = $("h5").html();
+        var subMoney = $(this).children("h5").html();
         sum -= parseFloat(subMoney.substr(1));
         $('.total .float-right').text(sum);
         $("#quantity").text(parseInt($("#quantity").text()) - 1);
-        $(this).remove();
+
+        name = $.trim($(this).children("h3").text().substr(1));
+
+        var number = parseInt($(this).children("#quantityInBag").text());
+        if (number > 1) $(this).children("#quantityInBag").text(number - 1);
+        else {
+            listOfProduct.splice(listOfProduct.indexOf(name), 1);
+            $(this).remove();
+        }
     });
 });
