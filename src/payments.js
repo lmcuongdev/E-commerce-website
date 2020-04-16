93235e2bf671ec71cf981d96233a15273dee5819
lmcuongdev <56270717+lmcuongdev@users.noreply.github.com>
2020-04-16 22:51:21 +0700

    checkout is now working

--- src/payments.js
+++ src/payments.js
@@ -1,8 +1,54 @@
 $(document).ready(function () {
   /* Set rates + misc */
-  var taxRate = 0.05;
-  var shippingRate = 15.0;
-  var fadeTime = 300;
+  const shippingRate = 6.9;
+  const fadeTime = 100;
+
+  function recalculateCart() {
+    let total = 0;
+
+    /* Sum up row totals */
+    $(".product-row").each((i, row) => {
+      total += parseFloat($(row).children(".product-subtotal").text());
+    });
+
+    /* Calculate totals */
+    let shipping = total > 0 ? shippingRate : 0;
+    let grandTotal = total + shipping;
+
+    /* Update totals display */
+    $("#total, #grand-total").fadeOut(fadeTime, () => {
+      $("#total").text(total.toFixed(2));
+      $("#cart-shipping").text(shipping.toFixed(2));
+      $("#grand-total").text(grandTotal.toFixed(2));
+      // not yet fixed
+      if (grandTotal == 0) {
+        $(".checkout").fadeOut(fadeTime);
+      } else {
+        $(".checkout").fadeIn(fadeTime);
+      }
+      $("#total, #grand-total").fadeIn(fadeTime);
+    });
+    console.log(total, grandTotal);
+  }
+
+  setTimeout(recalculateCart, 1000);
+  /* Update quantity */
+  function updateQuantity(quantityInput) {
+    /* Calculate line price */
+    let productRow = $(quantityInput).parent().parent();
+    let price = parseFloat(productRow.children(".product-price").text());
+    let quantity = $(quantityInput).val();
+    let linePrice = price * quantity;
+    /* Update line price display and recalc cart totals */
+    productRow.children(".product-subtotal").each((i, row) => {
+      $(row).fadeOut(fadeTime, () => {
+        console.log($(row).text());
+        $(row).text(linePrice.toFixed(2));
+        recalculateCart();
+        $(row).fadeIn(fadeTime);
+      });
+    });
+  }
 
   /* Assign actions */
   $(".product-quantity input").change(function () {
@@ -12,61 +58,15 @@ $(document).ready(function () {
   $(".product-removal button").click(function () {
     removeItem(this);
   });
-
   /* Recalculate cart */
-  function recalculateCart() {
-    var subtotal = 0;
-
-    /* Sum up row totals */
-    $(".product").each(function () {
-      subtotal += parseFloat($(this).children(".product-line-price").text());
-    });
-
-    /* Calculate totals */
-    var tax = subtotal * taxRate;
-    var shipping = subtotal > 0 ? shippingRate : 0;
-    var total = subtotal + tax + shipping;
-
-    /* Update totals display */
-    $(".totals-value").fadeOut(fadeTime, function () {
-      $("#cart-subtotal").html(subtotal.toFixed(2));
-      $("#cart-tax").html(tax.toFixed(2));
-      $("#cart-shipping").html(shipping.toFixed(2));
-      $("#cart-total").html(total.toFixed(2));
-      if (total == 0) {
-        $(".checkout").fadeOut(fadeTime);
-      } else {
-        $(".checkout").fadeIn(fadeTime);
-      }
-      $(".totals-value").fadeIn(fadeTime);
-    });
-  }
-
-  /* Update quantity */
-  function updateQuantity(quantityInput) {
-    /* Calculate line price */
-    var productRow = $(quantityInput).parent().parent();
-    var price = parseFloat(productRow.children(".product-price").text());
-    var quantity = $(quantityInput).val();
-    var linePrice = price * quantity;
-
-    /* Update line price display and recalc cart totals */
-    productRow.children(".product-line-price").each(function () {
-      $(this).fadeOut(fadeTime, function () {
-        $(this).text(linePrice.toFixed(2));
-        recalculateCart();
-        $(this).fadeIn(fadeTime);
-      });
-    });
-  }
 
   /* Remove item from cart */
-  function removeItem(removeButton) {
-    /* Remove row from DOM and recalc cart total */
-    var productRow = $(removeButton).parent().parent();
-    productRow.slideUp(fadeTime, function () {
-      productRow.remove();
-      recalculateCart();
-    });
-  }
+  // function removeItem(removeButton) {
+  //   /* Remove row from DOM and recalc cart total */
+  //   let productRow = $(removeButton).parent().parent();
+  //   productRow.slideUp(fadeTime, function () {
+  //     productRow.remove();
+  //     recalculateCart();
+  //   });
+  // }
 });
