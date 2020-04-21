a1798a2e4d39903848683a955465b95befa88559
lmcuongdev <56270717+lmcuongdev@users.noreply.github.com>
2020-04-22 01:20:33 +0700

    fix cart overflow

--- src/cart-button.js
+++ src/cart-button.js
@@ -15,18 +15,18 @@ $(document).ready(function () {
         var product = $(this).parent().parent().parent().siblings("div");
         var nameProduct = $.trim($(product).children("h3").text());
         var index = listOfProduct.indexOf(nameProduct);
-        if (index == -1) {
-            var img = $(this).parents(".product-top").children().children("img");
-            var sizeName = product.html().replace("h3", "h6").replace("h5", "h7");
-            $(".cart-list").prepend('<li>' + '<img src=' + img.attr('src') + '><span id="quantityInBag" class="float-right" style="font-size: larger; color: red;">1</span>' + sizeName + '</li>');
-            listOfProduct.push(nameProduct);
-        } else {
-            $(".cart-list li h6").each(function () {
-                if ($(this).text() == nameProduct) {
-                    $(this).siblings("span").text(parseInt($(this).siblings("span").text()) + 1);
-                }
-            });
-        }
+        // if (index == -1) {
+        var img = $(this).parents(".product-top").children().children("img");
+        var sizeName = product.html().replace("h3", "h6").replace("h5", "h7");
+        $(".cart-list").prepend('<li>' + '<img src=' + img.attr('src') + '><span id="quantityInBag" class="float-right" style="font-size: larger; color: red;">1</span>' + sizeName + '</li>');
+        listOfProduct.push(nameProduct);
+        // } else {
+        //     $(".cart-list li h6").each(function () {
+        //         if ($(this).text() == nameProduct) {
+        //             $(this).siblings("span").text(parseInt($(this).siblings("span").text()) + 1);
+        //         }
+        //     });
+        // }
         var addMoney = $(product).children("h5").html();
         sum += parseFloat(addMoney.substr(1));
         $('.total .float-right').text(sum);
@@ -34,6 +34,7 @@ $(document).ready(function () {
         $("#quantity").text(parseInt($("#quantity").text()) + 1);
     });
 
+    // remove item
     $(document).on("click", ".cart-list li[class!='total ']", function () {
         var subMoney = $(this).children("h7").html();
         sum -= parseFloat(subMoney.substr(1));
