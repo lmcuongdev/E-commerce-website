9890b652e23e547d3b2b474f84f41fa148e65694
lmcuongdev <56270717+lmcuongdev@users.noreply.github.com>
2020-04-22 21:22:41 +0700

    data sent

--- src/cart-button.js
+++ src/cart-button.js
@@ -7,28 +7,32 @@ $("#cart").on('click', function () {
 });
 
 $(document).ready(function () {
-    var sum = 0;
+    let sum = 0;
 
-    var listOfProduct = [];
+    // object list
+    let listOfProduct = [];
 
     $(".on-sale .fa-shopping-cart").on('click', function () {
-        var product = $(this).parent().parent().parent().siblings("div");
-        var nameProduct = $.trim($(product).children("h3").text());
-        var index = listOfProduct.indexOf(nameProduct);
-        if (index == -1) {
-            var img = $(this).parents(".product-top").children().children("img");
-            var sizeName = product.html().replace("h3", "h6").replace("h5", "h7");
+        let product = $(this).parent().parent().parent().siblings("div");
+        let nameProduct = $.trim($(product).children("h3").text());
+        let index = listOfProduct.map(obj => obj['name']).indexOf(nameProduct);
+        // get rid of $ sign
+        let price = parseFloat($(product).children("h5").html().substr(1));
+        if (index === -1) {
+            let img = $(this).parents(".product-top").children().children("img");
+            let sizeName = product.html().replace("h3", "h6").replace("h5", "h7");
             $(".cart-list").prepend('<li>' + '<img src=' + img.attr('src') + '><span class="quantityInBag float-right" style="font-size: larger; color: red;">1</span>' + sizeName + '</li>');
-            listOfProduct.push(nameProduct);
+            listOfProduct.push({ name: nameProduct, price: price, quantity: 1 });
         } else {
             $(".cart-list li h6").each(function () {
-                if ($(this).text() == nameProduct) {
-                    $(this).siblings("span").text(parseInt($(this).siblings("span").text()) + 1);
+                if ($(this).text() === nameProduct) {
+                    const quantityUpdate = parseInt($(this).siblings("span").text()) + 1;
+                    $(this).siblings("span").text(quantityUpdate);
+                    listOfProduct[index]['quantity'] = quantityUpdate;
                 }
             });
         }
-        var addMoney = $(product).children("h5").html();
-        sum += parseFloat(addMoney.substr(1));
+        sum += price;
         $('.total .float-right').text(sum);
 
         $("#quantity").text(parseInt($("#quantity").text()) + 1);
@@ -36,18 +40,21 @@ $(document).ready(function () {
 
     // remove item
     $(document).on("click", ".cart-list li[class!='total ']", function () {
-        var subMoney = $(this).children("h7").html();
+        let subMoney = $(this).children("h7").html();
         sum -= parseFloat(subMoney.substr(1));
         $('.total .float-right').text(sum);
         $("#quantity").text(parseInt($("#quantity").text()) - 1);
 
-        var namePro = $.trim($(this).children("h6").text().substr(1));
+        let namePro = $.trim($(this).children("h6").text());
 
-        var number = parseInt($(this).children(".quantityInBag").text());
-        if (number > 1) $(this).children(".quantityInBag").text(number - 1);
+        let curQuantity = parseInt($(this).children(".quantityInBag").text());
+        let productIndex = listOfProduct.map(obj => obj['name']).indexOf(namePro);
+        if (curQuantity > 1) {
+            $(this).children(".quantityInBag").text(curQuantity - 1);
+            listOfProduct[productIndex]['quantity'] = curQuantity - 1;
+        }
         else {
-            var indexOfPro = listOfProduct.indexOf(namePro);
-            listOfProduct.splice(indexOfPro, 1);
+            listOfProduct.splice(productIndex, 1);
             $(this).remove();
         }
     });
@@ -55,14 +62,8 @@ $(document).ready(function () {
 
     /*------------------------Go to checkout btn---------------*/
     $('.btn#complete').on('click', () => {
-        const x = $('.cart-list').children().slice(0, -1);
-        console.log(x);
-        const obj = []
-        localStorage.clear()
-        x.each((i, val) => {
-            console.log(val);
-            // localStorage[i] = val
-        })
-        console.log(listOfProduct);
+        // console.log(listOfProduct);
+        sessionStorage.setItem('cart', JSON.stringify(listOfProduct))
+        // console.log(sessionStorage);
     })
 });
