591771e900b51a6af04c300ab2a428a020806296
leduchuy225 <56461047+leduchuy225@users.noreply.github.com>
2020-05-01 09:24:58 +0700

    Thêm filter các sản phẩm yêu thích

--- src/filter.js
+++ src/filter.js
@@ -1,16 +1,32 @@
-$(document).ready(function () {
-  $(".filter-button").click(function () {
-    var value = $(this).attr("data-filter");
+$(document).ready(function() {
+    $(".filter-button").click(function() {
+        var value = $(this).attr("data-filter");
 
-    if (value == "all") {
-      $(".filter").show("1000");
-    } else {
-      $(".filter")
-        .not("." + value)
-        .hide("3000");
-      $(".filter")
-        .filter("." + value)
-        .show("3000");
-    }
-  });
+        if (value == "all") {
+            $(".filter").show("1000");
+        } else {
+            $(".filter")
+                .not("." + value)
+                .hide("3000");
+            $(".filter")
+                .filter("." + value)
+                .show("3000");
+        }
+    });
+});
+
+$(document).ready(function() {
+    var like = 0;
+
+    $(".fa-heart-o").click(function() {
+        if (like == 0) {
+            $(this).parents(".product-top").prepend('<i class="fa fa-heart fa-lg" style="right: 5%; margin-top: 20px; color:red; position: absolute;"></i>');
+            $(this).parents(".product").addClass("like");
+            like = 1;
+        } else {
+            $(this).parents(".product-top").children().remove("i");
+            $(this).parents(".product").removeClass("like");
+            like = 0;
+        }
+    });
 });
