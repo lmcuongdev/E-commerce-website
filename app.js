5609939a53d806e014b47d7bc9e5c57025a090f0
lmcuongdev <56270717+lmcuongdev@users.noreply.github.com>
2020-05-02 01:12:49 +0700

    change to handlebars

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 5000;
 const exphbs = require("express-handlebars");
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.engine("handlebars", exphbs());
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
-app.use(express.static(__dirname));
-
 app.get("/", (req, res) => {
-  // console.log(__dirname);
-  // res.sendFile(__dirname + "/index.html");
+  res.render("index", { title: "E-commerce" });
 });
+app.get("/payments", (req, res) => {
+  res.render("payments", { title: "Checkout", style: "payments.css" });
+});
+app.get("/signup", (req, res) => {
+  res.render("signup", { title: "Sign up", style: "signupstyle.css" });
+});
+app.get("/product", (req, res) => {
+  res.render("product", { title: "Product", style: "product.css" });
+});
+
+app.use(express.static(__dirname + "/public"));
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
