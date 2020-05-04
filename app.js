28f9a5921715da5fa170bfdc1afd602b783720fb
lmcuongdev <56270717+lmcuongdev@users.noreply.github.com>
2020-05-05 00:30:00 +0700

    Update database

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const exphbs = require("express-handlebars");
+const connection = require("./mysql");
+
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+// use handlebars
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+const products = [];
+connection.query(`SELECT * FROM products`, (err, rows, fields) => {
+  if (err) console.log(`Query error: ${err}`);
+  else {
+    products.push(...rows);
+  }
+});
+
+// make routes
 app.get("/", (req, res) => {
-  res.render("index", { title: "E-commerce" });
+  res.render("index", { title: "E-commerce", products });
 });
 app.get("/payments", (req, res) => {
   res.render("payments", { title: "Checkout", style: "payments.css" });
