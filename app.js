c6a3356c964e41d23ae25bd78d8ee1774d6d28de
lmcuongdev <56270717+lmcuongdev@users.noreply.github.com>
2020-05-15 23:27:54 +0700

    login bassically work

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const exphbs = require("express-handlebars");
+const session = require("express-session");
 const products = require("./productList");
 
 // routes
 const paymentRoute = require("./routes/payments/payments");
 const signupRoute = require("./routes/signup/signup");
+const loginRoute = require("./routes/login/login");
 const productRoute = require("./routes/product/product");
 
 const PORT = process.env.PORT || 5000;
@@ -25,6 +27,15 @@ app.set("view engine", "handlebars");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 },
+  })
+);
+
 // make routes
 app.get("/", (req, res) => {
   res.render("index", { title: "E-commerce", products });
@@ -33,14 +44,11 @@ app.post("/", (req, res) => {
   res.status(200).json({ products });
 });
 
+app.use("/login", loginRoute);
 app.use("/payments", paymentRoute);
-
 app.use("/signup", signupRoute);
-
 app.use("/product", productRoute);
-
 app.use(express.static(__dirname + "/public"));
-
 app.use((req, res, next) => {
   res.status(404).render("404", { title: "Page not found", style: "404.css" });
 });
---
+++ routes/login/login.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const db = require("../../mysql");
+const loginRouter = express.Router();
+
+loginRouter.post("/", (req, res) => {
+  console.log(req.body);
+  const { username, password } = req.body;
+  db.query(
+    `select * from users where username='${username}' and password='${password}'`,
+    (err, rows) => {
+      if (err) console.log(err);
+      else {
+        // if username and password is correct
+        if (rows.length) {
+          req.session.userId = rows[0].id;
+          req.session.user = rows[0];
+          console.log(rows[0].id);
+          console.log("Logged in");
+          console.log(req.protocol + "://" + req.get("host") + req.originalUrl);
+          res.status(200).json({ headers: req.headers, session: req.session });
+        } else {
+          // FIXME: this one is not done
+          res.render("404");
+        }
+      }
+    }
+  );
+});
+
+module.exports = loginRouter;
