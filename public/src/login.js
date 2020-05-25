5ed79fb601a9f63ef2e1ba6ecbcc346a149acd76
lmcuongdev <56270717+lmcuongdev@users.noreply.github.com>
2020-05-25 20:33:46 +0700

    minor refactor

--- public/src/login.js
+++ public/src/login.js
@@ -1,11 +1,3 @@
-$("#sign-up").click(function () {
-  $("#form-group").css("display", "block");
-});
-
-$("#form-group span").click(function () {
-  $("#form-group").css("display", "none");
-});
-
 $("#form-group").submit((ev) => {
   ev.preventDefault();
   //get data here
